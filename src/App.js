2666473a5708bc4b67e8f1300964cee78e540985
khaled aljundi <kaledj007@gmail.com>
2018-03-11 15:17:55 +0200

    set up ui semantic library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
 
-const App = () => <div> Hello World</div>;
+
+const App = () => (
+  <div className="ui container">
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  </div>);
 
 
 export default App;
