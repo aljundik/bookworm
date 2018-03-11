3bbeb858d315970d424fd906b5aea680a5745f83
khaled aljundi <kaledj007@gmail.com>
2018-03-11 18:58:26 +0200

    create login form

---
+++ src/components/Forms/LoginForm.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Form, Button, } from 'semantic-ui-react';
+import Validator from 'validator';
+import PropTypes from 'prop-types';
+
+import InlineValidate from './InlineValidate/InlineValidate';
+
+
+class LoginForm extends React.Component {
+    state = {
+      data: {
+        email: '',
+        password: '',
+      },
+      loading: false,
+      errors: {},
+
+    };
+
+    onChange = (e) => {
+      this.setState({
+        data: { ...this.state.data, [e.target.name]: e.target.value, },
+      });
+    }
+
+    onSubmit = () => {
+      const errors = this.validate(this.state.data);
+      this.setState({ errors, });
+      this.props.submit(this.state.data);
+      if (!Object.keys(errors)) {
+        this.props.submit(this.state.data);
+      }
+    };
+
+    validate= (data) => {
+      const errors = {};
+      const { email, password, } = data;
+      if (!Validator.isEmail(email)) errors.email = 'Email is invalid';
+      if (password.length < 1) errors.passowrd = "Password shouldn't be embty";
+
+      return errors;
+    }
+
+    render() {
+      const { data, errors, } = this.state;
+      return (
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field error={!!errors.email}>
+            <label htmlFor="email"> Email
+              <input
+                id="email"
+                type="email"
+                placeholder="example@example.com"
+                name="email"
+                value={data.email}
+                onChange={this.onChange}
+              />
+            </label>
+            { errors.email && <InlineValidate error={errors.email} /> }
+          </Form.Field>
+          <Form.Field error={!!errors.email}>
+            <label htmlFor="password"> Password
+              <input
+                id="password"
+                type="password"
+                placeholder="secure Email"
+                name="password"
+                value={data.passowrd}
+                onChange={this.onChange}
+              />
+            </label>
+            { errors.passowrd && <InlineValidate error={errors.passowrd} /> }
+          </Form.Field>
+          <Button primary> Login </Button>
+
+        </Form>
+      );
+    }
+}
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
+
+export default LoginForm;
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import LoginForm from '../Forms/LoginForm';
 
-const HomePage = () => (
-  <div>
-    <h1>Login Page</h1>
-  </div>);
+class LoginPage extends React.Component {
+  submit = (data) => {
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <LoginForm submit={this.submit} />
+      </div>);
+  }
+}
 
 
-export default HomePage;
+export default LoginPage;
